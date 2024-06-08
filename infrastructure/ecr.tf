e3101d0830f5fed46412246cf18593cf38c60429
Blaz Peternel <blaz.peternel@trams.co.uk>
2024-06-08 12:07:59 +0100

    Update ecr.tf

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr" {
-    name = "ECR_${var.powertools_service_name}_${var.environment}"
+    name = "ecr_${var.powertools_service_name}_${var.environment}"
     image_tag_mutability = var.ecr_image_tag_mutability
 
   image_scanning_configuration {
