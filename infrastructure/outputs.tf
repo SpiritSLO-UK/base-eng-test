d1af7f46134750853aba8648a9ab973e4a67be32
Blaz Peternel <blaz.peternel@trams.co.uk>
2024-06-10 21:42:32 +0100

    Roll-back

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,12 +2,3 @@ output "api_gateway_url" {
   value       = aws_apigatewayv2_api.event_handler.api_endpoint
   description = "URL of the API Gateway"
 }
-
-output "ecr_repository_url" {
-    value = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/ecr_${var.powertools_service_name}_${var.environment}"
-    description = "URL of the ECR repository"
-}
-
-output "Test_output" {
-    value = "This works 60% of the time, every time!"
-}
