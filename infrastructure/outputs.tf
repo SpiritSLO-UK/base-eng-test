a6cbd2cf119c98964551ef8fb5268e9f86a1f9ce
Blaz Peternel <blaz.peternel@trams.co.uk>
2024-06-10 21:23:09 +0100

    Variables MORE

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,3 +2,8 @@ output "api_gateway_url" {
   value       = aws_apigatewayv2_api.event_handler.api_endpoint
   description = "URL of the API Gateway"
 }
+
+output "ecr_repository_url" {
+    value = local.ecr_repository_url
+    description = "URL of the ECR repository"
+}
